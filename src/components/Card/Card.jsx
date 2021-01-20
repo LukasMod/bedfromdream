dd9caa965a72b44969c5ce22b9a685a5ed1e4547
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-20 20:19:07 +0100

    Card component, styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusSquare, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
 import './style.scss';
 const Card = ({ data }) => {
+  const { urlThumb, name, description, cost } = data;
+  const [numberOfNights, setNumberOfNights] = useState(0);
+
+  const handleSubtract = () => {
+    setNumberOfNights(numberOfNights - 1);
+  };
+
+  const handleAdd = () => {
+    setNumberOfNights(numberOfNights + 1);
+  };
+
   return (
     <div className="Card" onClick={() => {}}>
       <div className="Card__img-wrapper">
-        <img className="Card__img" src={data.urlThumb} alt={data.name} />
+        <img className="Card__img" src={urlThumb} alt={name} />
       </div>
       <div className="Card__info-wrapper">
-        <h3 className="Card__title">{data.name}</h3>
-        <p className="Card__description">{data.description}</p>
+        <h3 className="Card__title">{name}</h3>
+        <p className="Card__description">{description}</p>
+      </div>
+      <div className="Card__cost-wrapper">
+        <span className="Card__cost">{cost} $/night</span>
+        {numberOfNights === 0 || (
+          <span className="Card__cost--sum">
+            total: {(numberOfNights * cost).toFixed(2)} $
+          </span>
+        )}
       </div>
       <div className="Card__actions-wrapper">
-        <button className="Card__subtract">-1</button>
-        <span>0</span>
-        <button className="Card__add">+1</button>
+        {numberOfNights === 0 || (
+          <FontAwesomeIcon
+            icon={faMinusSquare}
+            className="Card__icon"
+            onClick={handleSubtract}
+          />
+        )}
+        <span className="Card__nights-sum">{numberOfNights}</span>
+        <FontAwesomeIcon
+          icon={faPlusSquare}
+          className="Card__icon"
+          onClick={handleAdd}
+        />
       </div>
     </div>
   );
